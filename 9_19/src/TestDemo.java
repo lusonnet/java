49bbe4edbcc4f5b770cb8d18cc108337a3b132ab
sonnet <lulu_2001@foxmail.com>
2021-09-19 23:37:23 +0800

    提交代码

---
+++ 9_19/src/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * Created by lulu
+ * Description:
+ * User: Administrator
+ * Date: 2021-09-19
+ * Time: 22:26
+ */
+public class TestDemo {
+    public static void main1(String[] args) {
+        String str1 = "hellobit";
+        System.out.println(str1);
+        String str2 = new String("hello");
+    }
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello");
+        System.out.println(str1==str2);
+    }
+}
