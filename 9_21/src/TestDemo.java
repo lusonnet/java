c0db2fb2ec197dd582e7a6262610806e535a51dc
sonnet <lulu_2001@foxmail.com>
2021-09-21 23:12:37 +0800

    提交代码

---
+++ 9_21/src/TestDemo.java
@@ -0,0 +1,28 @@
+/**
+ * Created by lulu
+ * Description:
+ * User: Administrator
+ * Date: 2021-09-21
+ * Time: 12:51
+ */
+public class TestDemo {
+    /*public static void main1(String[] args) {
+        String str = "abcd";
+        //String->stringBuffer、StringBuilder
+        //1.构造方法
+        StringBuilder sb = new StringBuilder(str);
+        //2.append()
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(str);
+    }*/
+
+
+        public static void main(String[] args) {
+        /*//stringBuffer、StringBuilder->String
+        StringBuilder sb = new StringBuilder("abc");
+        String s = sb.toString();
+        System.out.println(s);*/
+
+            System.out.println("hello");
+        }
+}
