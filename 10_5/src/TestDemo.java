1612324a9193f92ef1d9fdf59877bad339989215
sonnet <lulu_2001@foxmail.com>
2021-10-05 23:39:55 +0800

    提交代码

---
+++ 10_5/src/TestDemo.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+
+/**
+ * Created by lulu
+ * Description:
+ * User: Administrator
+ * Date: 2021-10-05
+ * Time: 10:23
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
