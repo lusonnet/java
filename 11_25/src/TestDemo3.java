63123e42eda29bb5f2815efe375820a6e2bd1164
sonnet <lulu_2001@foxmail.com>
2021-11-27 14:18:10 +0800

    提交代码

--- 11_25/src/TestDemo3.java
+++ 11_25/src/TestDemo3.java
@@ -9,4 +9,16 @@ import java.util.Date;
  * Date: 2021-11-25
  * Time: 21:10
  */
-
+public class TestDemo3 {
+    public static void main(String[] args) {
+        String s = "hello";
+        try {
+            s = s+" world";
+            s.toUpperCase();
+            String[] a = s.split("o");
+            System.out.println(a.length);
+        } catch (Exception e) {
+            System.out.print(s);
+        } finally {
+            System.out.print(s);
+        }}}
